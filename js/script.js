2cd60a94fc2b152e7618d0e7028d1c54018cef24
FredericKanni <kannif@ymail.com>
2019-09-16 12:17:07 +0400

    grid reussi

--- js/script.js
+++ js/script.js
@@ -11,19 +11,22 @@ let uncheck = '<input type="checkbox" name="" value="">';
 let check ='<input type="checkbox" name="" value="" checked>';
 
 
-ligne1.innerHTML = uncheck  ;
+//ligne1.innerHTML = uncheck  ;
 
 
 
-//largeur de la ligne
+
 for(let i = 0; i< largeur;i++){
-    ligne1.innerHTML = ligne1.innerHTML + uncheck;
-     //nombre de ligne
-
-
+  //  ligne1.innerHTML = ligne1.innerHTML + uncheck;
+     //largeur de la ligne 
+     for(let j = 0; j< longueur;j++){
+        ligne1.innerHTML =  ligne1.innerHTML + uncheck ;
+        
+     }
+    //on a creer une ligne il faut passer a la ligne n est pas ?
+     ligne1.innerHTML =  ligne1.innerHTML +'<br>';
+     //on a passe a la ligne on va creer la ligne i+1
 }
 
 
-for(let j = 0; j< longueur;j++){
-    ligne1.innerHTML =  '<br>' + ligne1.innerHTML ;
- }
+
